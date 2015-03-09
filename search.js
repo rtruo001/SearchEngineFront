851bef0d039771490ff6427aa58a6260af3c7124
Randy Truong <rtruo001@ucr.edu>
2015-03-09 02:03:08 -0700

    More updates to website

--- search.js
+++ search.js
@@ -7,21 +7,12 @@ function ShowBox()
         $("#testFade").slideToggle("slow");
 }
 
-function DoThis() {
-  $(function()
-  {
-          $.get("http://localhost:8080/Testingpart2/TestingServlet2", function(data, textStatus) {
-                  alert("Done, with the following status: " + textStatus + ". Here is the response: " + data);
-          });
-  });
-}
-
 function DisplayOnHTML(json) {
   console.log("In DisplayOnHTML " + JSON.stringify(json));  
   document.body.style.backgroundColor = "#CCFFCC";
   
-   var searchList = document.getElementById("listOfSearches");
-   for (var i = 0; i < json.results.length; i++) {
+  var searchList = document.getElementById("listOfSearches");
+  for (var i = 0; i < json.results.length; i++) {
     var newSearchListItem = document.createElement("li");
     var searchListValue = document.createTextNode("Title " + i + ": " + json.results[i].title);
     newSearchListItem.appendChild(searchListValue);
@@ -31,6 +22,10 @@ function DisplayOnHTML(json) {
 
 //$(document).ready(function() {
   $("#Button").click(function(event){
+    var query = document.getElementById("inputBar");
+    queryValue = query.value;
+    console.log("QUERY VALUE: " + queryValue);
+
     $.getJSON("http://localhost:8080/Testingpart2/TestingServlet2", function(json, textStatus) {
       //var json = JSON.parse(data);
       console.log("Here is the response: " + JSON.stringify(json)); 
